f970d3917d0f69fbf27ccea7f30a4cf266d673fa
AdeleGropplero <adele.gdt@hotmail.com>
2025-03-18 10:27:34 +0100

    inizio profilo.

--- src/components/c_CapPersonale/Capsula.jsx
+++ src/components/c_CapPersonale/Capsula.jsx
@@ -68,9 +68,10 @@ function Capsula() {
       api
         .delete(`/capsula/${id}`)
         .then((res) => {
+          console.log(res);
           if (res.status === 200) {
             alert("Capsula eliminata con successo!");
-            navigate("/le-mie-capsule"); // Reindirizza l'utente alla lista delle capsule
+            navigate(`/le-mie-caps/:id`); // Reindirizza l'utente alla lista delle capsule
           }
         })
         .catch((error) => {
@@ -85,15 +86,30 @@ function Capsula() {
 
   // Funzione per gestire il caricamento dei file
   const handleFileUpload = (e, fileType) => {
-    const selectedFiles = Array.from(
-      e.target.files
-    ); /* è una collezione di file selezionati dall'utente tramite il campo di input. Si tratta di un oggetto di tipo FileList, che è simile a un array, ma non ha tutte le caratteristiche di un array JavaScript standard (per esempio, non ha metodi come forEach). */
+    const selectedFiles = Array.from(e.target.files).map((file) => ({
+      id: crypto.randomUUID(), // Genera un ID univoco per ogni file. ATTENZIONE non ha nulla a che vedere con il db. è solo un id temporaneo.
+      name: file.name, // Nome originale del file
+      type: file.type, // Tipo MIME del file (es. "image/png", "video/mp4")
+      file // Mantiene il file originale
+    })); /* è una collezione di file selezionati dall'utente tramite il campo di input. Si tratta di un oggetto di tipo FileList, che è simile a un array, ma non ha tutte le caratteristiche di un array JavaScript standard (per esempio, non ha metodi come forEach). */
     setFormData((prev) => ({
       ...prev,
-      [`new${fileType}`]: [
-        ...prev[`new${fileType}`],
-        ...selectedFiles
-      ] /* Qui stai usando la **notazione di accesso dinamico** alle proprietà di un oggetto. La stringa new${fileType}diventa il nome della proprietà che contiene i file appena caricati. Ad esempio, sefileTypeè"media", la proprietà sarà newMedia`. */
+      newMedia: [
+        ...prev.newMedia,
+        ...selectedFiles.filter(
+          (f) => f.type.startsWith("image/") || f.type.startsWith("video/")
+        )
+      ],
+      newTextFiles: [
+        ...prev.newTextFiles,
+        ...selectedFiles.filter(
+          (f) =>
+            f.type === "application/pdf" ||
+            f.name.endsWith(".docx") ||
+            f.name.endsWith(".doc") ||
+            f.name.endsWith(".txt")
+        )
+      ]
     }));
   };
 
@@ -138,11 +154,28 @@ function Capsula() {
       });
   };
 
+  const removeFile = (fileId, type) => {
+    setFormData((prev) => {
+      if (type === "image") {
+        return {
+          ...prev,
+          newMedia: prev.newMedia.filter((file) => file.id !== fileId) // Rimuove il file con l'ID specificato
+        };
+      } else if (type === "text") {
+        return {
+          ...prev,
+          newTextFiles: prev.newTextFiles.filter((file) => file.id !== fileId) // Rimuove il file con l'ID specificato
+        };
+      }
+      return prev; // Se il tipo non è valido, restituisci lo stato precedente
+    });
+  };
+
   return (
     <>
       <MyNavBar />
       <Container className="mt-4">
-        <Card className="shadow-sm p-4">
+        <Card className="shadow-sm p-4 mb-5">
           <Card.Body>
             <Card.Title className="text-center">{capsula.title}</Card.Title>
             <Card.Text className="text-center">
@@ -211,78 +244,143 @@ function Capsula() {
               // Se la capsula non è ancora apribile, permetti la modifica
               //----------------------------------------------------------------
               <>
-                <p className="text-center text-warning">
-                  🔒 La capsula non può ancora essere aperta. Puoi modificarla.
+                <p className="text-center text-danger">
+                  🔒 La capsula non può ancora essere aperta. <br />
+                  Se vuoi aggiungi ricordi!.
                 </p>
+                <div className="d-flex justify-content-between mt-4">
+                  {/* Bottone per eliminare la capsula */}
+                  <Button
+                    className="bottone-crea"
+                    onClick={() => navigate(`/le-mie-caps/:id`)}
+                  >
+                    <img src="/iconeGenerali/arrowL.svg" alt="torna indietro" />
+                  </Button>
 
-                <Button
-                  variant="primary"
-                  onClick={() => setEditMode(!editMode)}
-                >
-                  {editMode ? "Annulla Modifica" : "Modifica Capsula"}
-                </Button>
+                  <Button
+                    className="bottone-crea"
+                    onClick={() => setEditMode(!editMode)}
+                  >
+                    {editMode ? "Annulla Modifica" : "Modifica Capsula"}
+                  </Button>
 
+                  {/* Bottone per eliminare la capsula */}
+                  <Button className="bottone-crea" onClick={handleDelete}>
+                    <img src="/iconeGenerali/trash.svg" alt="cestino" />
+                  </Button>
+                </div>
                 {editMode && (
-                  <Form className="mt-4" onSubmit={handleSaveChanges}>
-                    {/* Modifica Titolo */}
-                    <Form.Group className="mb-3">
-                      <Form.Label>Titolo</Form.Label>
-                      <Form.Control
-                        type="text"
-                        defaultValue={capsula.title}
-                        onChange={(e) =>
-                          setFormData({ ...formData, title: e.target.value })
-                        }
-                      />
-                    </Form.Group>
+                  <>
+                    <Form className="mt-4" onSubmit={handleSaveChanges}>
+                      {/* Modifica Titolo */}
+                      <Form.Group className="mb-3">
+                        <Form.Label>Titolo</Form.Label>
+                        <Form.Control
+                          type="text"
+                          defaultValue={capsula.title}
+                          onChange={(e) =>
+                            setFormData({ ...formData, title: e.target.value })
+                          }
+                        />
+                      </Form.Group>
 
-                    {/* Modifica Messaggio */}
-                    <Form.Group className="mb-3">
-                      <Form.Label>Messaggio</Form.Label>
-                      <Form.Control
-                        as="textarea"
-                        rows={3}
-                        defaultValue={capsula.message}
-                        onChange={(e) =>
-                          setFormData({ ...formData, message: e.target.value })
-                        }
-                      />
-                    </Form.Group>
+                      {/* Modifica Messaggio */}
+                      <Form.Group className="mb-3">
+                        <Form.Label>Messaggio</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          rows={3}
+                          defaultValue={capsula.message}
+                          onChange={(e) =>
+                            setFormData({
+                              ...formData,
+                              message: e.target.value
+                            })
+                          }
+                        />
+                      </Form.Group>
 
-                    {/* Upload Media */}
-                    <Form.Group className="mb-3">
-                      <Form.Label>Aggiungi Foto/Video</Form.Label>
-                      <Form.Control
-                        type="file"
-                        multiple
-                        accept="image/*,video/*"
-                        onChange={(e) => handleFileUpload(e, "Media")}
-                      />
-                    </Form.Group>
+                      {/* Upload di immagini/video e file di testo*/}
+                      <Form.Group className="mb-3 form-text">
+                        <Form.Label>Carica foto/video/file di testo</Form.Label>
+                        <Form.Control
+                          type="file"
+                          multiple /* Permette di selezionare più file. */
+                          accept="image/*,video/*,.pdf,.doc,.docx,.txt" // Accetta immagini, video e file di testo
+                          onChange={handleFileUpload}
+                          id="fileInput" //questo mi serve per recuperare l'elemento con il getElementbyId
+                        />
+                        {/* Bottone personalizzato */}
+                        <Button
+                          className="bottone-file"
+                          onClick={() =>
+                            document.getElementById("fileInput").click()
+                          } //è un modo per "simulare" un click sull'input type="file" nascosto, quando l'utente preme il bottone.
+                        >
+                          📁 Seleziona File
+                        </Button>
+                      </Form.Group>
 
-                    {/* Upload File di Testo */}
-                    <Form.Group className="mb-3">
-                      <Form.Label>Aggiungi File di Testo</Form.Label>
-                      <Form.Control
-                        type="file"
-                        multiple
-                        accept=".pdf,.doc,.docx,.txt"
-                        onChange={(e) => handleFileUpload(e, "TextFiles")}
-                      />
-                    </Form.Group>
-
-                    <Button variant="success" type="submit ">
-                      Salva Modifiche
-                    </Button>
-                  </Form>
+                      <Button className="bottone-crea" type="submit ">
+                        Salva Modifiche
+                      </Button>
+                    </Form>
+                    {formData.newMedia.length > 0 ||
+                    formData.newTextFiles.length > 0 ? (
+                      <div className="form-capsula-div mb-3 mt-3">
+                        {/* Mostra i file caricati */}
+                        <h6>I tuoi caricamenti</h6>
+                        {formData.newMedia.length > 0 && (
+                          <ul className="mt-1">
+                            {formData.newMedia.map((file, index) => (
+                              <li
+                                key={index}
+                                className="d-flex align-items-center"
+                              >
+                                <Button
+                                  size="sm"
+                                  className="me-2 cancel-btn bottone-crea"
+                                  onClick={() => removeFile(file.id, "image")}
+                                >
+                                  X
+                                </Button>
+                                {/* Visualizza l'immagine */}
+                                <img
+                                  src={URL.createObjectURL(file.file)} // Usa file.file per ottenere l'URL dell'immagine
+                                  alt={file.name}
+                                  style={{
+                                    width: 40,
+                                    height: 40,
+                                    objectFit: "cover"
+                                  }}
+                                  className="me-2"
+                                />
+                              </li>
+                            ))}
+                          </ul>
+                        )}
+                        {formData.newTextFiles.length > 0 && (
+                          <ul className="">
+                            {formData.newTextFiles.map((file, index) => (
+                              <li key={index}>
+                                <Button
+                                  size="sm"
+                                  className="me-2 mb-1 cancel-btn bottone-crea"
+                                  onClick={() => removeFile(file.id, "text")}
+                                >
+                                  X
+                                </Button>
+                                <span>{file.name}</span>
+                              </li>
+                            ))}
+                          </ul>
+                        )}
+                      </div>
+                    ) : null}
+                  </>
                 )}
               </>
             )}
-
-            {/* Bottone per eliminare la capsula */}
-            <Button variant="danger" className="mt-4" onClick={handleDelete}>
-              Elimina Capsula
-            </Button>
           </Card.Body>
         </Card>
       </Container>
